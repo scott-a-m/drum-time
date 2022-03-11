a6e801d7db9e8749dfe4ebec0c5b06b3d40524eb
[Scott] <[scott.a.mitchell1@hotmail.com]>
2022-03-11 17:50:17 +0800

    updated contact info

--- src/App.jsx
+++ src/App.jsx
@@ -240,7 +240,7 @@ class App extends React.Component {
             />
           </a>
           <a
-            href="https://twitter.com/_scott_a_m"
+            href="https://twitter.com/scotts-dev"
             target="_blank"
             rel="noreferrrer"
           >
